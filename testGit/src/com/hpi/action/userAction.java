7de34a3f6afecff9587b609081076b2a74d47585
dhj1212 <376196834@qq.com>
2021-03-22 16:42:31 +0800

    # WARNING: head commit changed in the meantime
    77

---
+++ testGit/src/com/hpi/action/userAction.java
@@ -0,0 +1,12 @@
+package com.hpi.action;
+
+public class userAction
+{
+
+	public static void main(String[] args)
+	{
+		// TODO 自动生成的方法存根
+
+	}
+
+}
--- wsClient1/src/com/cxfwb/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://cxfWB.com/")
-package com.cxfwb;
